293e292196b6bd0785e40f3d1138b2ca32e09182
Nadir Cohen <nadircs11@gmail.com>
2023-08-09 03:09:07 +0300

    fix qoriq rcw url

--- package/nxp/qoriq-rcw/qoriq-rcw.mk
+++ package/nxp/qoriq-rcw/qoriq-rcw.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 QORIQ_RCW_VERSION = $(call qstrip,$(BR2_PACKAGE_HOST_QORIQ_RCW_VERSION))
-QORIQ_RCW_SITE = https://source.codeaurora.org/external/qoriq/qoriq-components/rcw
+QORIQ_RCW_SITE = https://github.com/nxp-qoriq/rcw
 QORIQ_RCW_SITE_METHOD = git
 QORIQ_RCW_LICENSE = BSD-3-Clause
 QORIQ_RCW_LICENSE_FILES = LICENSE
